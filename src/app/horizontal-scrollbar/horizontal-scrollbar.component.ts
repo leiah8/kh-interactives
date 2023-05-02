a33b6e96b543442fa7cb0f9de6a282159ede9c25
leiah8 <leiah.nay8@gmail.com>
2023-05-02 16:11:45 -0400

    smth

--- src/app/horizontal-scrollbar/horizontal-scrollbar.component.ts
+++ src/app/horizontal-scrollbar/horizontal-scrollbar.component.ts
@@ -95,57 +95,76 @@ function main(setup) {
   });
 } */
 
+//TO DO
+//add boxes around numbers 
+//make slider width adjust to ratio between line and numbers shown 
+//make the range of numbers adjustable (change boxwidth)
+
 function main2(setup) {
   let start_text = document.getElementById("start_text") as HTMLElement
   let end_text = document.getElementById("end_text") as HTMLElement
   let redCircle = document.getElementById("redCircle") as HTMLElement
+  
+  let minX = 0 
+  let maxX = 78.75 
+  gsap.set(redCircle, {scaleX : setup.rangeNum / 2, x : minX})
+  
 
-  let startBox = document.getElementById("startNum") as HTMLElement
+  let startBox = document.getElementById("startBox") as HTMLElement
   let startNum = document.getElementById("startNumText") as HTMLElement
+  let numbers = document.getElementById("numbers") as HTMLElement
 
   let box = document.getElementById("box") as HTMLElement
-
-
   let layer = document.getElementById("layer1") as HTMLElement
 
-  let boxWidth = 10 // change??
-  let diff = 6.12 ////////////////change 
+  let boxWidth = 10 //change
+  let diff = 5.85 //change 
 
+  console.log((Number(setup.lineMax-setup.lineMin)+1)*boxWidth)
+  
+  //TO DO: FIX SPACING
+  //let boxW = Math.round(startBox.getBoundingClientRect().width);
+  //let boxX = Math.round(startBox.getBoundingClientRect().x);
+  //let numX = Math.round(startNum.getBoundingClientRect().x);
 
-  let numberBoxes = [startNum]
-  startNum.textContent = (setup.lineMin-2).toString()
-  let currentNum = setup.lineMin - 1
-  for (var i = setup.lineMin-1; i <= Number(setup.lineMax)+2; i++) {
-    let node = startNum.cloneNode(true)
+  startNum.textContent = (setup.lineMin).toString()
+  let currentNum = Number(setup.lineMin)+1
+  for (var i = setup.lineMin+1; i <= Number(setup.lineMax); i++) {
+    let nodeBox = startBox.cloneNode(true) as HTMLElement
+    numbers.appendChild(nodeBox)
+    gsap.set(nodeBox, {x : (i)*boxWidth })
+
+    let node = startNum.cloneNode(true) as HTMLElement
     node.textContent = currentNum.toString()
-    layer.appendChild(node)
-    gsap.set(node, {x : (i+2)*boxWidth })
-    numberBoxes.push(node as HTMLElement)
+    numbers.appendChild(node)
+    gsap.set(node, {x : (i)*boxWidth })
+
+    //let numW = node.getBoundingClientRect().width
+
+    //gsap.set(node, {x : (numW/2 + numX - (boxW/2 + boxX)) + (i+2)*boxWidth })
+    
     currentNum++;
   }
 
-  //redraw 
   layer.removeChild(box)
   layer.appendChild(box)
 
-  let length = setup.lineMax - setup.lineMin
 
-  //let currentNum = Math.floor((setup.lineMax - setup.lineMin) / 2)
-  let maxX = 82
-  let minX = 0
-
-
-  let tl = gsap.timeline()
   gsap.registerPlugin(Draggable);
 
   start_text.textContent = setup.lineMin
   end_text.textContent = setup.lineMax
   Draggable.create(redCircle, {type: "x", bounds:{minX : minX, maxX : maxX}, 
     onDrag : function() {
-      //gsap.to(startBox, {x : diff*(-this.x)+(0*boxWidth) });
-      for(var i = 0; i < numberBoxes.length; i++) {
-        gsap.to(numberBoxes[i], {x : diff*(-this.x)+(i*boxWidth) });
-      }
+      gsap.to(numbers, {x : diff*(-this.x)})
     }
+  });
+
+  let numbersMinX = -((Number(setup.lineMax-setup.lineMin))*(boxWidth-(diff/7.5))) // change
+
+  Draggable.create(numbers, {type: "x", bounds : {minX : numbersMinX, maxX : 0}, 
+    onDrag : function() {
+      gsap.to(redCircle, {x : (-this.x)/diff})
+    }, 
   });
 }
