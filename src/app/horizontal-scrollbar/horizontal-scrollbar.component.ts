c9a4bc114a98cadbf31f9f804f3e46a6bb50583c
leiah8 <leiah.nay8@gmail.com>
2023-05-03 15:54:19 -0400

    ul version

--- src/app/horizontal-scrollbar/horizontal-scrollbar.component.ts
+++ src/app/horizontal-scrollbar/horizontal-scrollbar.component.ts
@@ -5,7 +5,8 @@ import { gsap, Draggable } from "gsap/all";
 @Component({
   selector: 'app-horizontal-scrollbar',
   //templateUrl: './horizontal-scrollbar.component.html',
-  templateUrl: './hs.html',
+  //templateUrl: './hs2.html',
+  templateUrl: './hs3.html',
   styleUrls: ['./horizontal-scrollbar.component.css']
 })
 export class HorizontalScrollbarComponent implements AfterViewInit {
@@ -35,7 +36,7 @@ export class HorizontalScrollbarComponent implements AfterViewInit {
     //const interactive = horizontalScrollAPI(els, setup); 
 
     
-    main2(setup)
+    main3(setup)
   }
 
 }
@@ -95,38 +96,43 @@ function main(setup) {
   });
 } */
 
-//TO DO
-//make the range of numbers adjustable (change boxwidth)
-
+/*
 function main2(setup) {
   let start_text = document.getElementById("start_text") as HTMLElement
   let end_text = document.getElementById("end_text") as HTMLElement
   let redCircle = document.getElementById("redCircle") as HTMLElement
+  let line = document.getElementById("line") as HTMLElement
   
+  gsap.set(redCircle, {scaleX : setup.rangeNum / 2, x : 0})
+
+  //TO DO
+  let over = (redCircle as any as SVGGraphicsElement).getBBox().width * (setup.rangeNum / 2)  / 3.5 //change with app
   let minX = 0 
-  let maxX = 78.75 
-  gsap.set(redCircle, {scaleX : setup.rangeNum / 2, x : minX})
-  
+  let maxX = (line as any as SVGGraphicsElement).getBBox().width - (redCircle as any as SVGGraphicsElement).getBBox().width * (setup.rangeNum / 2) + over
+
+  console.log(maxX)
 
   let startBox = document.getElementById("startBox");
   let startNum = document.getElementById("startNumText");
   let numbers = document.getElementById("numbers") as HTMLElement
 
+
   let box = document.getElementById("box") as HTMLElement
   let layer = document.getElementById("layer1") as HTMLElement
 
-  //TO DO
-  let boxWidth = 10 //change with app 
-  let diff = 12.2 //change with app 
-  
-  //5.85 for 0 - 50 with boxWidth 10 and rangeNum 5
-  //12.2 for 0 - 100 with boxWidth 10 and rangeNum 5
-  
-  let boxW = Math.round((startBox as any as SVGGraphicsElement).getBBox().width);
+  //MAX: 5 nums with boxwidth 10
+  let boxWidth = 50 / Number(setup.rangeNum) 
+
+  let boxW = Math.round((startBox as any as SVGGraphicsElement).getBBox().width * (boxWidth/10));
   let boxX = Math.round((startBox as any as SVGGraphicsElement).getBBox().x);
   let numX = Math.round((startNum as any as SVGGraphicsElement).getBBox().x);
 
+  gsap.set(startBox, {scaleX : boxWidth / 10});
   startNum.textContent = (setup.lineMin).toString()
+  let numW = (startNum as any as SVGGraphicsElement).getBBox().width
+  gsap.set(startNum, {x : -(numW/2 + numX - (boxW/2 + boxX))-0.5 })
+
+
   let currentNum = Number(setup.lineMin)+1
   for (var i = setup.lineMin+1; i <= Number(setup.lineMax); i++) {
     let nodeBox = startBox.cloneNode(true)
@@ -148,6 +154,7 @@ function main2(setup) {
   layer.removeChild(box)
   layer.appendChild(box)
 
+  let diff = (numbers as any as SVGGraphicsElement).getBBox().width / (line as any as SVGGraphicsElement).getBBox().width
 
   gsap.registerPlugin(Draggable);
 
@@ -158,14 +165,68 @@ function main2(setup) {
       gsap.to(numbers, {x : diff*(-this.x)})
     }
   });
-
-  //TO DO FIX THE MAX
-  let n = 31 //7.5 for 0 - 15 with boxWidth 10 an rangeNum 5
-  let numbersMinX = -((Number(setup.lineMax-setup.lineMin))*(boxWidth-(diff/n))) // change
+  
+  let numbersMinX = -((numbers as any as SVGGraphicsElement).getBBox().width - (setup.rangeNum - 0.25)*boxWidth) // change with app
 
   Draggable.create(numbers, {type: "x", bounds : {minX : numbersMinX, maxX : 0}, 
     onDrag : function() {
       gsap.to(redCircle, {x : (-this.x)/diff})
     }, 
   });
+}
+*/
+
+function main3(setup){
+  //set up the numbers
+  var first = document.querySelector('li')
+  first.textContent = Number(setup.lineMin).toString();
+
+  let numbers = document.querySelector('ul')
+
+  for(var i = Number(setup.lineMin)+1; i < Number(setup.lineMax)+1; i++) {
+    var node = document.createElement('li');
+    node.appendChild(document.createTextNode(i.toString()));
+    node.setAttribute("style", "scroll-snap-align: center; display: flex; justify-content: center; align-items: center; background: #fff; border-radius: 8px; font-size : 20px;font-family : 'Poppins';")
+    numbers.appendChild(node);
+
+  }
+  const list = document.querySelectorAll('.hs'); 
+
+  list.forEach(el => {
+    const n = el.children.length;
+    (el as HTMLElement).style.setProperty('--total', n.toString());
+    (el as HTMLElement).style.setProperty('--boxSize', (67 * (5 / setup.rangeNum)).toString() +"px");
+  });
+
+  //set up the draggables
+  let start_text = document.getElementById("start_text") as HTMLElement
+  let end_text = document.getElementById("end_text") as HTMLElement
+  let redCircle = document.getElementById("redCircle") as HTMLElement
+  let line = document.getElementById("line") as HTMLElement
+  
+  gsap.set(redCircle, {scaleX : setup.rangeNum / 2, x : 0})
+
+  //TO DO
+  let over = (redCircle as any as SVGGraphicsElement).getBBox().width * (setup.rangeNum / 2)  / 3.5 //change with app
+  let minX = 0 
+  let maxX = (line as any as SVGGraphicsElement).getBBox().width - (redCircle as any as SVGGraphicsElement).getBBox().width * (setup.rangeNum / 2) + over
+
+  //TO DO FIX
+  let diff = (73.5 * (setup.lineMax - setup.lineMin) * (5 / setup.rangeNum) / (line as any as SVGGraphicsElement).getBBox().width)
+
+  const items = document.querySelectorAll('.hs'); 
+  start_text.textContent = setup.lineMin
+  end_text.textContent = setup.lineMax
+
+  gsap.registerPlugin(Draggable);
+  Draggable.create(redCircle, {type: "x", bounds:{minX : minX, maxX : maxX}, 
+    onDrag : function() {
+      gsap.to(numbers, {x : diff*(-this.x)})
+      //items.forEach(el => {
+      //  gsap.to(el, {x : diff*(-this.x)})
+      //})
+    }
+  });
+
+
 }
