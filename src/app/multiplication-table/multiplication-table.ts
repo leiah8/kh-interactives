18b2d35b0562fb1e760fcdb1ba18c1bce95e30ca
leiah8 <leiah.nay8@gmail.com>
2023-06-07 16:45:32 -0400

    multtable

--- src/app/multiplication-table/multiplication-table.ts
+++ src/app/multiplication-table/multiplication-table.ts
@@ -14,63 +14,48 @@ export class MultiplicationTableClass {
 
         var cols = ""
         for(var i = 0; i < m; i++) {
-            cols += "10vw "
+            cols += "5vw "
         }
 
         var listStyle = "grid-column: 1 / -1; list-style: none; padding: 0; display: grid; grid-gap: 10px; overflow-x : auto; scroll-snap-type: x proximity;"
-        listStyle += "grid-template-columns: " + cols + "; grid-template-rows: 50px;"
+        listStyle += "grid-template-columns: " + cols + "; grid-template-rows: 2vw;"
         
-       
-        var elStyle = "scroll-snap-align: center; display: flex; justify-content: center; align-items: center; background: #fff; border-radius: 8px; font-size : 14px;font-family : 'Poppins';"
+        var elStyle = "scroll-snap-align: center; display: flex; justify-content: center; align-items: center; background: #fff; border-radius: 8px; font-size : 14px;font-family : 'Poppins'; height : 3vw"
 
-        // var lists = document.querySelectorAll('.row'); 
-        // var i = 1;
-        // lists.forEach(row => {
-        //     for (var j = 1; j <= m; j++) { 
-        //         var el = document.createElement('li');
-        //         el.appendChild(document.createTextNode((i*j).toString()));
-        //         el.setAttribute("style", elStyle)
-        //         row.appendChild(el);
-
-        //     }
-        //     i++;
-        //   });
-
-        // var row = document.createElement('ul');
-        // row.setAttribute("style", listStyle)
-        // this.table.appendChild(row);
-
-        // for (var j = 1; j <= m; j++) { 
-        //     var el = document.createElement('li');
-        //     el.appendChild(document.createTextNode((j).toString()));
-        //     el.setAttribute("style", elStyle)
-        //     row.appendChild(el);
-    
-        // }
-        
-        for (var i = 1; i <= n; i++) {
-            var row = document.createElement('ul');
-            //row.appendChild(document.createTextNode(i.toString()));
-            row.setAttribute("style", listStyle)
-            this.table.appendChild(row);
-            //row.style.setProperty('--scrollbar-color', "red");
-            //row.style.overflow = 'hidden';
-            //row.style.width = '100%'
-
-            //TO DO GET RID OF SCROLLBARS
-            
-
-            //row.style.scrollbarTrackColour.setAttribute("color", "transparent")
-
+        //add to pre exisiting rows 
+        var lists = document.querySelectorAll('.row'); 
+        var i = 1;
+        lists.forEach(row => {
             for (var j = 1; j <= m; j++) { 
-
                 var el = document.createElement('li');
                 el.appendChild(document.createTextNode((i*j).toString()));
                 el.setAttribute("style", elStyle)
                 row.appendChild(el);
 
             }
-        }
+            i++;
+          });
+
+        
+        //creat new rows
+        
+        // for (var i = 1; i <= n; i++) {
+        //     var row = document.createElement('ul');
+        //     row.setAttribute("style", listStyle)
+        //     //row.classList.add("row")
+        //     this.table.appendChild(row);
+
+        //     //TO DO GET RID OF SCROLLBARS
+            
+        //     for (var j = 1; j <= m; j++) { 
+
+        //         var el = document.createElement('li');
+        //         el.appendChild(document.createTextNode((i*j).toString()));
+        //         el.setAttribute("style", elStyle)
+        //         row.appendChild(el);
+
+        //     }
+        // }
             
 
     }
