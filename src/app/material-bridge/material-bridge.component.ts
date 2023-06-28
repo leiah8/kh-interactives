717be3dcd97755005a80cf43f2fa3817c019ef66
leiah8 <leiah.nay8@gmail.com>
2023-06-28 17:01:17 -0400

    material bridge changes

--- src/app/material-bridge/material-bridge.component.ts
+++ src/app/material-bridge/material-bridge.component.ts
@@ -8,24 +8,33 @@ import { MaterialBridgeAPI, GameInput } from './material-bridge';
   styleUrls: ['./material-bridge.component.css']
 })
 export class MaterialBridgeComponent implements AfterViewInit {
-  // @ViewChild("arena") public arena?: ElementRef<HTMLElement>;
+  @ViewChild("arena") public arena?: ElementRef<HTMLElement>;
   @ViewChild("input") public input?: ElementRef<HTMLElement>;
   @ViewChild("inputImg") public inputImg?: ElementRef<HTMLElement>;
   @ViewChild("inputSize") public inputSize?: ElementRef<HTMLElement>;
-
+  @ViewChild("inputPieces") public inputPieces?: ElementRef<HTMLElement>;
+  @ViewChild("orderBtn") public orderBtn?: ElementRef<HTMLElement>;
+  @ViewChild("playBtn") public playBtn?: ElementRef<HTMLElement>;
+  @ViewChild("boat") public boat?: ElementRef<HTMLElement>;
+  
   constructor() { }
 
   ngAfterViewInit(): void {
 
     const setup = {
-      // arena : this.arena.nativeElement,
+      arena : this.arena.nativeElement,
       input : this.input.nativeElement,
       inputImg : this.inputImg.nativeElement,
       inputSize : this.inputSize.nativeElement,
+      inputPieces : this.inputPieces.nativeElement,
+      orderBtn : this.orderBtn.nativeElement,
+      playBtn : this.playBtn.nativeElement,
+      boat : this.boat.nativeElement
 
     }
 
     const g0 = {
+      bridgeArr : [[0,1,1,0], [0]]
 
     } as GameInput
 
