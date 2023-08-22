32736362da1b1a803f11b9b1e93af9530625f14c
leiah2 <leiah.nay@knowledgehook.com>
2023-08-22 13:56:44 -0400

    clip mask stuff

--- src/app/clip-mask/clip-mask.component.ts
+++ src/app/clip-mask/clip-mask.component.ts
@@ -14,31 +14,44 @@ export class ClipMaskComponent implements AfterViewInit {
 
     // https://css-tricks.com/clipping-masking-css/ 
     // https://greensock.com/forums/topic/13615-clip-path-or-mask-animation/
-    var r = document.getElementById("useRect")
+
+    /*
+    var r = document.getElementById("smth")
     var tl = gsap.timeline()
 
     tl.to(r, {duration : 2})
 
     //not smooth
-    tl.to(r, {clipPath: "circle(40px at center)", duration : 2})
+    tl.to(r, {clipPath: "circle(300px at center)", duration : 2})
+    tl.to(r, {clipPath: "circle(100px at center)", duration : 2})
+
     tl.to(r, {clipPath: "circle(30px at center)", duration : 2})
 
     //circle to circle is smooth
 
-    // tl.to(r, {clipPath: "inset(10px 20px 30px 40px", duration : 2})
-    //not smooth 
-
     tl.to(r, {clipPath: "polygon(50% 0%, 80% 10%, 100% 35%, 100% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)", duration : 2});
 
-    tl.to(r, {clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)", duration : 2});
+    tl.to(r, {clipPath: "polygon(25% 0%, 75% 0%, 75% 0%, 100% 50%, 100% 50%, 75% 100%, 75% 100%, 25% 100%, 25% 100%, 0% 50%)", duration : 2});
   
-    tl.to(r, {clipPath: "polygon(50% 0%, 0% 100%, 100% 100%, 100% 100%)", duration : 2});
-    tl.to(r, {clipPath: "polygon(50% 0%, 0% 100%, 100% 100%, 100% 50%)", duration : 2});
+    tl.to(r, {clipPath: "polygon(50% 0%, 50% 0%, 0% 100%, 100% 100%)", duration : 2});
+    tl.to(r, {clipPath: "polygon(100% 0%, 0% 0%,  0% 100%, 100% 100%)", duration : 2});
     //last transition is smooth (look at last point)
 
     //polygon to polygon is not smooth (reverts back to corner)
     //points need to able to morph positions 
     //if there is not the same number of points, points go to 0,0
+
+    */
+
+
+
+    var r3 = document.getElementById("rect3")
+    var tl = gsap.timeline()
+    tl.to(r3, {duration : 2});
+    tl.to(r3, {scaleX : 2, duration : 2});
+
+
+    //animating the inner elements does change the clippath on the group
   }
 
 }
